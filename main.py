2e2c8939ebf5841bc10e681cd4802b497fddfb63
Anselmos <anselmos@users.noreply.github.com>
2022-02-18 14:38:01 +0100

    Updates script for lambda usage.

--- main.py
+++ main.py
@@ -2,13 +2,26 @@ import requests
 import sys, getopt
 
 ENEA_SHORTAGE_LINK = "http://www.wylaczenia-eneaoperator.pl/page_print.php?rejon=17"
-def find_outage_by_text(text):
-    res = requests.get(ENEA_SHORTAGE_LINK)
-    return text in res.text
+
+def get_outage_page():
+    return requests.get(ENEA_SHORTAGE_LINK)
+
+
+def find_outage_by_text(page, text):
+    return text in page.text
+
+def find_all(list_texts):
+    page = get_outage_page()
+
+    outages = {}
+    for text in list_texts:
+        outages[text] = find_outage_by_text(page, text)
+
+    return outages
+
 
 def main(argv):
-    text_search = argv[0]
-    found = find_outage_by_text(text_search)
+    found = find_all(argv)
     print(found)
 
 if __name__ =="__main__":
